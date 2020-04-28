a9649d5fbc80699ea277a74afbebe742c3e74cf5
userwgoodintention <60634240+userwgoodintention@users.noreply.github.com>
2020-04-28 16:21:23 -0400

    update

--- introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript/js/buscar-pacientes.js
@@ -1,22 +1,30 @@
 var botaoAdicionar = document.querySelector("#buscar-paciente");
 
 botaoAdicionar.addEventListener("click", function () {
-console.log("buscando pacientes....")	
+	console.log("buscando pacientes....")	
 
-var xhr = new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+	xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
 
-xhr.addEventListener("load", function () {
-	var resposta = xhr.responseText;
-	var pacientes = JSON.parse(resposta);
 
-	pacientes.forEach(function (paciente) {
-		adicionaPacienteNaTabela(paciente);
-	})
-});
+	xhr.addEventListener("load", function () {
+		var erroAjax = document.querySelector("#erro-ajax");
 
-xhr.send();
+		if (xhr.status == 200 ) {
+			erroAjax.classList.add("invisivel");
+			var resposta = xhr.responseText;
+			var pacientes = JSON.parse(resposta);
+
+			pacientes.forEach(function (paciente) {
+				adicionaPacienteNaTabela(paciente);
+			});
+		}else {
+			erroAjax.classList.remove("invisivel");
+		}
+	});
+
+	xhr.send();
 
 });
